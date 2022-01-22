2bc16aca71e6adceed9e44f2ba5f5b4e30e84c08
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-01-22 14:41:03 +0900

    Create 폰켓몬.js

---
+++ 폰켓몬.js
@@ -0,0 +1,10 @@
+const solution = (nums) => {
+    let answer = 0;
+    const set = [...new Set(nums)];
+    answer = set.length;
+    
+    if(answer > nums.length/2) {
+        answer = nums.length/2;
+    }
+    return answer;
+}
