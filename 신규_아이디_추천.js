1b2d916389f2aab0787dc0659fe5e0775c7a722f
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-02-06 00:45:58 +0900

    신규_아이디_추천.js 합격><

--- 신규_아이디_추천.js
+++ 신규_아이디_추천.js
@@ -1,16 +1,15 @@
 function solution(new_id) {
     let id = new_id.toLowerCase().replace(/[^\w-._]+/g,'').split('\.').filter((e) => e !== '').join('.');
-    if(id.length === 0) {
-        for(let i=0; i < new_id.length; i++) {
-            id+='a';
-        }
-    } else if(id.length >= 16) {
+    if(id.length === 0) id = 'a';
+    else if(id.length >= 16) {
         id = id.substr(0,15);
         id = id.split('\.').filter((e) => e !== '').join('.');
-    } else if( id.length <= 2 ) {
-        for(let i = 0; i < 3-id.length; i++) {
+    }
+    if( id.length <= 2 ) {
+        for(let i = 0; i <= 3-id.length; i++) {
             id+=id.charAt(id.length-1);
         }
     }
     return id;
 }
+
