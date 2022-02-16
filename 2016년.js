6d78c58b0bc224e4a7f29cd70c0579b159371b67
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-02-16 21:21:19 +0900

    2022-02-16 2016년

--- 2016년.js
+++ 2016년.js
@@ -1,4 +1,4 @@
 const solution = (a, b) => {
     const day = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-    return day[new Date(2016, a-1, b).getDay()];
+    return day[ new Date(2016, a-1, b).getDay() ];
 }
