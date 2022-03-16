4188fa67e0f411320da0127bd450c177bb70943c
punchlips97 <64599394+ekdldksp123@users.noreply.github.com>
2022-03-16 19:47:30 +0900

    2022-03-16 두 개 뽑아서 더하기 통과~

---
+++ 두_개_뽑아서_더하기.js
@@ -0,0 +1,23 @@
+const solution = (numbers) => {
+    const answer = [];
+    const arr = getCombinations(numbers, 2).map(v => v.reduce((acc,cur) => acc+cur,0));
+    
+    for(const e of arr) {
+        if(!answer.includes(e)) answer.push(e);
+    }
+    return answer.sort((x,y) => x-y);
+}
+
+  const getCombinations = function (arr, selectNumber) {
+    const results = [];
+    if (selectNumber === 1) return arr.map((el) => [el]); 
+
+    arr.forEach((fixed, index, origin) => {
+      const rest = origin.slice(index + 1); 
+      const combinations = getCombinations(rest, selectNumber - 1); 
+      const attached = combinations.map((el) => [fixed, ...el]); 
+      results.push(...attached); 
+    });
+
+    return results;
+}
